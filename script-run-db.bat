db9fe9a2faa65ee9b35a99b88be5d75b60449e65
Drojanx <drojanxha@gmail.com>
2022-05-01 20:23:11 +0200

    Subida ficheros Dockerfile y docker-compose

---
+++ script-run-db.bat
@@ -0,0 +1,6 @@
+start
+pushd %~dp0
+docker run -d -p 3309:3306 --name bookshelter-db -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_DATABASE=bsapi -e MYSQL_USER=bsuser -e MYSQL_PASSWORD=100316 mysql
+pause
+popd
+pause
--- src/main/java/com/svalero/bookshelterapi/config/Config.java
+++ src/main/java/com/svalero/bookshelterapi/config/Config.java
@@ -3,12 +3,19 @@ package com.svalero.bookshelterapi.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class Config {
+public class Config extends WebMvcConfigurerAdapter {
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
